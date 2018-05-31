38576b6f3b52c26b95a475f3be2882fd1f785649
bc <brian.cohn@usc.edu>
2018-05-30 22:35:25 -0700

    #1 working version for pypi

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ p = path.abspath(path.dirname(__file__))
 with open(path.join(p, 'README.md')) as f:
     README = f.read()
 setup(name='ftpsconnector',
-      version='0.1.7',
-      description='Initial connector code for pensieve',
+      version='0.2.0',
+      description='Initial FTPS Binary File Upload/Dowload connector code for Pensieve',
       long_description=README,
       long_description_content_type="text/markdown",
       url='http://github.com/bc/ftpsconnector',
