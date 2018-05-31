23232eeb3628dc6d4369fc078ecc084f70c73198
bc <brian.cohn@usc.edu>
2018-05-30 20:49:24 -0700

    #1 moved requirements for travis

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
+
 from setuptools import setup
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+
+p = path.abspath(path.dirname(__file__))
+with open(path.join(p, 'README.md')) as f:
+    README = f.read()
 setup(name='ftpsconnector',
-      version='0.1.3',
+      version='0.1.4',
       description='Initial connector code for pensieve',
-      long_description=long_description,
-	  long_description_content_type="text/markdown",
+      long_description=README,
+      long_description_content_type="text/markdown",
       url='http://github.com/bc/ftpsconnector',
       author='Brian Cohn',
       author_email='brian.cohn@usc.edu',
